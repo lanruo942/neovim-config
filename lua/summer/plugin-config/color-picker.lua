1be73fb6aa18c0ebb59d08b9d583ac654eab4bb0
Summer Lee <lee@summer.today>
2022-08-10 23:17:13 +0800

    update color-picker config file

--- lua/summer/plugin-config/color-picker.lua
+++ lua/summer/plugin-config/color-picker.lua
@@ -11,7 +11,7 @@ vim.keymap.set("i", "<A-c>", "<cmd>PickColorInsert<cr>", opts)
 -- vim.keymap.set("n", "<leader>[", "<cmd>ConvertHEXandRGB<cr>", opts)
 -- vim.keymap.set("n", "<leader>]", "<cmd>ConvertHEXandHSL<cr>", opts)
 
-require("color-picker").setup({ -- for changing icons & mappings
+color_picker.setup({ -- for changing icons & mappings
 	-- ["icons"] = { "ﱢ", "" },
 	-- ["icons"] = { "ﮊ", "" },
 	-- ["icons"] = { "", "ﰕ" },
@@ -19,7 +19,7 @@ require("color-picker").setup({ -- for changing icons & mappings
 	-- ["icons"] = { "", "" },
 	["icons"] = { "ﱢ", "" },
 	["border"] = "rounded", -- none | single | double | rounded | solid | shadow
-	["keymap"] = { -- mapping example:
+	-- ["keymap"] = { -- mapping example:
 		-- default keymaps: https://github.com/ziontee113/color-picker.nvim
 		-- h and l will increment the color slider value by 1.
 		-- u and i / a and d / A and D will increment the color slider value by 5.
@@ -30,7 +30,7 @@ require("color-picker").setup({ -- for changing icons & mappings
 		-- If your slider is on the 4th line (the same line as the color output preview), when you increment / set a color value, it will apply that change to all 3 sliders above it.
 		-- gu will select the 1st and 2nd sliders, gd will select the 2nd and 3rd sliders, gm will select the 1st and 3rd sliders.
 		-- Press x will deselect the slider group.
-	},
+	-- },
 })
 
 vim.cmd([[hi FloatBorder guibg=NONE]]) -- if you don't want weird border background colors around the popup.
