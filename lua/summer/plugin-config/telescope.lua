8f37504b1dffb620910eefdbbb8a526bac84e22a
Summer Lee <lee@summer.today>
2022-08-10 23:19:10 +0800

    update telescope config to enable insert mode keymap

--- lua/summer/plugin-config/telescope.lua
+++ lua/summer/plugin-config/telescope.lua
@@ -13,36 +13,36 @@ telescope.setup({
 		path_display = { "smart" },
 		initial_mode = "insert",
 		mappings = {
-			-- i = {
-			-- 	["<C-n>"] = actions.cycle_history_next,
-			-- 	["<C-p>"] = actions.cycle_history_prev,
-   --
-			-- 	["<C-j>"] = actions.move_selection_next,
-			-- 	["<C-k>"] = actions.move_selection_previous,
-   --
-			-- 	["<C-c>"] = actions.close,
-   --
-			-- 	["<Down>"] = actions.move_selection_next,
-			-- 	["<Up>"] = actions.move_selection_previous,
-   --
-			-- 	["<CR>"] = actions.select_default,
-			-- 	["<C-x>"] = actions.select_horizontal,
-			-- 	["<C-v>"] = actions.select_vertical,
-			-- 	["<C-t>"] = actions.select_tab,
-   --
-			-- 	["<C-u>"] = actions.preview_scrolling_up,
-			-- 	["<C-d>"] = actions.preview_scrolling_down,
-   --
-			-- 	["<PageUp>"] = actions.results_scrolling_up,
-			-- 	["<PageDown>"] = actions.results_scrolling_down,
-   --
-			-- 	["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-			-- 	["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-			-- 	["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-			-- 	["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-			-- 	["<C-l>"] = actions.complete_tag,
-			-- 	["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-			-- },
+			i = {
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+
+				["<C-c>"] = actions.close,
+
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+			},
 			n = {
 				["<esc>"] = actions.close,
 				["<CR>"] = actions.select_default,
