62c05a9d4dc35d2cf8c4d86746cbd3c141b7833a
Summer Lee <lee@summer.today>
2023-02-27 00:24:20 +0800

    add git blame config

--- lua/summer/plugin-config/lualine.lua
+++ lua/summer/plugin-config/lualine.lua
@@ -4,6 +4,16 @@ if not status_ok then
 	return
 end
 
+--[[ vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text ]]
+vim.g.gitblame_message_template = " <author> ﮮ <date> ﴬ <summary>"
+--[[ vim.g.gitblame_message_when_not_committed = "Oh please, commit this !" ]]
+vim.g.gitblame_date_format = "%r"
+local git_blame_status_ok, git_blame = pcall(require, "gitblame")
+if not git_blame_status_ok then
+	vim.notify("git_blame not found!")
+	return
+end
+
 vim.cmd([[ command! Copilot statusline ]])
 
 lualine.setup({
@@ -20,6 +30,8 @@ lualine.setup({
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = {
 			"filename",
+			require("auto-session-library").current_session_name,
+			--[[ { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }, ]]
 			-- {
 			-- 	"lsp_progress",
 			-- 	spinner_symbols = { " ", " ", " ", " ", " ", " " },
