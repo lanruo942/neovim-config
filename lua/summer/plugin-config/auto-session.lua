b55ab5864b816d3a30df35a0c5c30290056c6143
Summer Lee <lee@summer.today>
2023-03-16 11:58:09 +0800

    update auto-session configuration

--- lua/summer/plugin-config/auto-session.lua
+++ lua/summer/plugin-config/auto-session.lua
@@ -9,10 +9,13 @@ auto_session.setup({
 	auto_session_enable_last_session = false,
 	auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
 	auto_session_enabled = true,
-	auto_save_enabled = nil,
-	auto_restore_enabled = nil,
+	auto_save_enabled = true,
+	auto_restore_enabled = true,
 	auto_session_suppress_dirs = { "~/", "~/Downloads/", "/", "/etc", "/usr" },
 	auto_session_use_git_branch = nil,
 	-- the configs below are lua only
 	bypass_session_save_file_types = nil,
+	cwd_change_handling = nil,
 })
+
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
