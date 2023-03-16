10e3b4834fc91ce1c36854a3db59d0a6f0071f2b
Summer Lee <lee@summer.today>
2023-03-16 11:48:38 +0800

    remove no used keymap

--- lua/summer/keymaps.lua
+++ lua/summer/keymaps.lua
@@ -65,7 +65,6 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
--- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap(
 	"n",
 	"<A-f>",
